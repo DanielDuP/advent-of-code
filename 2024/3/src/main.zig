aff415440be53b5754931ddc09769a9257accc53
DanielDuP <54242914+DanielDuP@users.noreply.github.com>
2024-12-13 23:26:25 -0500

    refactor for readability

--- 2024/3/src/main.zig
+++ 2024/3/src/main.zig
@@ -12,49 +12,54 @@ pub fn main() !void {
 
     var buf: [1024]u8 = undefined;
     var total: i64 = 0;
-    file_read: while (try reader.readUntilDelimiterOrEof(&buf, ')')) |line| {
-        if (line.len < SHORTEST_POSSIBLE_MUL) continue :file_read;
-        var cursor = line.len - 1;
-        var first_argument = std.ArrayList(u8).init(std.heap.page_allocator);
-        defer first_argument.deinit();
-        var second_argument = std.ArrayList(u8).init(std.heap.page_allocator);
-        defer second_argument.deinit();
-        var is_second_arg = false;
-        while (cursor > 0) {
-            const current_char = line[cursor];
-            switch (current_char) {
-                '0'...'9' => {
-                    if (is_second_arg) {
-                        try second_argument.insert(0, current_char);
-                    } else {
-                        try first_argument.insert(0, current_char);
-                    }
-                },
-                ',' => {
-                    if (is_second_arg) {
-                        continue :file_read;
-                    }
-                    is_second_arg = true;
-                },
-                '(' => {
-                    if (cursor < 3) {
-                        continue :file_read;
-                    }
-                    if (!std.mem.eql(u8, line[cursor - 3 .. cursor], "mul")) {
-                        continue :file_read;
-                    }
-                    if (!is_second_arg) {
-                        continue :file_read;
-                    }
-                    total += try mul(first_argument.items, second_argument.items);
-                    continue :file_read;
-                },
-                else => continue :file_read,
-            }
-            cursor -= 1;
-        }
+    while (try reader.readUntilDelimiterOrEof(&buf, ')')) |line| {
+        if (line.len < SHORTEST_POSSIBLE_MUL) continue;
+        total += (try processLine(line)) orelse 0;
     }
     std.debug.print("Total: {}\n", .{total});
+}
+
+fn processLine(line: []const u8) !?i64 {
+    var cursor = line.len - 1;
+    var first_argument = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer first_argument.deinit();
+    var second_argument = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer second_argument.deinit();
+    var is_second_arg = false;
+
+    while (cursor > 0) {
+        const current_char = line[cursor];
+        switch (current_char) {
+            '0'...'9' => {
+                if (is_second_arg) {
+                    try second_argument.insert(0, current_char);
+                } else {
+                    try first_argument.insert(0, current_char);
+                }
+            },
+            ',' => {
+                if (is_second_arg) {
+                    return null;
+                }
+                is_second_arg = true;
+            },
+            '(' => {
+                if (cursor < 3) {
+                    return null;
+                }
+                if (!std.mem.eql(u8, line[cursor - 3 .. cursor], "mul")) {
+                    return null;
+                }
+                if (!is_second_arg) {
+                    return null;
+                }
+                return try mul(first_argument.items, second_argument.items);
+            },
+            else => return null,
+        }
+        cursor -= 1;
+    }
+    return null;
 }
 
 pub fn mul(first_argument: []const u8, second_argument: []const u8) !i64 {
