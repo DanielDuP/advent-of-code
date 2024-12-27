f50b4cab8068180b0fb9850317186155ddf19f13
DanielDuP <54242914+DanielDuP@users.noreply.github.com>
2024-12-26 22:00:34 -0500

    small refinements and changes

--- 2024/5/src/main.zig
+++ 2024/5/src/main.zig
@@ -2,53 +2,59 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 const NumberType = u32;
+const Rule = [2]NumberType;
+const Sequence = []const NumberType;
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
 
     var buffered_reader = std.io.bufferedReader(file.reader());
     const reader = buffered_reader.reader();
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    var rules = std.ArrayList([2]NumberType).init(allocator);
+    var rules = std.ArrayList(Rule).init(allocator);
     defer rules.deinit();
-    var validSequences = std.ArrayList([]const NumberType).init(allocator);
-    defer validSequences.deinit();
-    var invalidSequences = std.ArrayList([]const NumberType).init(allocator);
-    defer invalidSequences.deinit();
+    var valid_sequences = std.ArrayList(Sequence).init(allocator);
+    defer valid_sequences.deinit();
+    var invalid_sequences = std.ArrayList(Sequence).init(allocator);
+    defer invalid_sequences.deinit();
 
+    try processInputFile(reader, &rules, &valid_sequences, &invalid_sequences, allocator);
+
+    const valid_sum = calculateMiddleTermSum(valid_sequences);
+    std.debug.print("Middle Term Sum of Valid Sequences: {}\n", .{valid_sum});
+
+    const corrected_sum = try calculateCorrectedMiddleTermSum(invalid_sequences, rules, allocator);
+    std.debug.print("Middle Term Sum of Corrected Sequences: {}\n", .{corrected_sum});
+}
+
+fn processInputFile(reader: anytype, rules: *std.ArrayList(Rule), valid_sequences: *std.ArrayList(Sequence), invalid_sequences: *std.ArrayList(Sequence), allocator: std.mem.Allocator) !void {
     var buf: [4096]u8 = undefined;
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (std.mem.indexOfScalar(u8, line, '|')) |_| {
             const rule = try parseRule(line);
             try rules.append(rule);
         } else {
-            try processSequence(line, rules, &validSequences, &invalidSequences, allocator);
+            try processSequence(line, rules.items, valid_sequences, invalid_sequences, allocator);
         }
     }
-
-    const validSum = calculateMiddleTermSum(validSequences);
-    std.debug.print("Middle Term Sum of Valid Sequences: {}\n", .{validSum});
-
-    const correctedSum = try calculateCorrectedMiddleTermSum(invalidSequences, rules, allocator);
-    std.debug.print("Middle Term Sum of Corrected Sequences: {}\n", .{correctedSum});
 }
 
-fn parseRule(input: []const u8) ![2]NumberType {
+fn parseRule(input: []const u8) !Rule {
     var iter = std.mem.splitScalar(u8, input, '|');
     const first = iter.next() orelse return error.InvalidInput;
     const second = iter.next() orelse return error.InvalidInput;
-    return [2]NumberType{
+    return Rule{
         try std.fmt.parseInt(NumberType, std.mem.trim(u8, first, " "), 10),
         try std.fmt.parseInt(NumberType, std.mem.trim(u8, second, " "), 10),
     };
 }
 
-fn isValidRule(sequence: []const NumberType, rule: [2]NumberType) bool {
+fn isValidRule(sequence: Sequence, rule: Rule) bool {
     var first_index: ?usize = null;
     var second_index: ?usize = null;
 
@@ -63,7 +69,7 @@ fn isValidRule(sequence: []const NumberType, rule: [2]NumberType) bool {
     return (first_index == null or second_index == null) or (first_index.? < second_index.?);
 }
 
-fn enforceRule(sequence: []const NumberType, rule: [2]NumberType, allocator: std.mem.Allocator) !?[]NumberType {
+fn enforceRule(sequence: Sequence, rule: Rule, allocator: std.mem.Allocator) !?Sequence {
     var result = try allocator.dupe(NumberType, sequence);
     errdefer allocator.free(result);
 
@@ -79,8 +85,7 @@ fn enforceRule(sequence: []const NumberType, rule: [2]NumberType, allocator: std
 
         if (first_index != null and second_index != null) {
             if (first_index.? > second_index.?) {
-                result[first_index.?] = sequence[second_index.?];
-                result[second_index.?] = sequence[first_index.?];
+                std.mem.swap(NumberType, &result[first_index.?], &result[second_index.?]);
                 return result;
             } else {
                 return null;
@@ -91,7 +96,7 @@ fn enforceRule(sequence: []const NumberType, rule: [2]NumberType, allocator: std
     return null;
 }
 
-fn processSequence(input: []const u8, rules: std.ArrayList([2]NumberType), validSequences: *std.ArrayList([]const NumberType), invalidSequences: *std.ArrayList([]const NumberType), allocator: std.mem.Allocator) !void {
+fn processSequence(input: []const u8, rules: []const Rule, valid_sequences: *std.ArrayList(Sequence), invalid_sequences: *std.ArrayList(Sequence), allocator: std.mem.Allocator) !void {
     var numbers = std.ArrayList(NumberType).init(allocator);
     defer numbers.deinit();
 
@@ -105,18 +110,15 @@ fn processSequence(input: []const u8, rules: std.ArrayList([2]NumberType), valid
         return;
     }
 
-    const isValid = for (rules.items) |rule| {
+    const is_valid = for (rules) |rule| {
         if (!isValidRule(numbers.items, rule)) break false;
     } else true;
 
-    if (isValid) {
-        try validSequences.append(try numbers.toOwnedSlice());
-    } else {
-        try invalidSequences.append(try numbers.toOwnedSlice());
-    }
+    const sequence = try numbers.toOwnedSlice();
+    try if (is_valid) valid_sequences.append(sequence) else invalid_sequences.append(sequence);
 }
 
-fn calculateMiddleTermSum(sequences: std.ArrayList([]const NumberType)) NumberType {
+fn calculateMiddleTermSum(sequences: std.ArrayList(Sequence)) NumberType {
     var sum: NumberType = 0;
     for (sequences.items) |sequence| {
         sum += sequence[sequence.len / 2];
@@ -124,23 +126,32 @@ fn calculateMiddleTermSum(sequences: std.ArrayList([]const NumberType)) NumberTy
     return sum;
 }
 
-// this is really ugly, but hey, it worked first time I tried it, so I'm moving on
-fn calculateCorrectedMiddleTermSum(sequences: std.ArrayList([]const NumberType), rules: std.ArrayList([2]NumberType), allocator: std.mem.Allocator) !NumberType {
+fn calculateCorrectedMiddleTermSum(sequences: std.ArrayList(Sequence), rules: std.ArrayList(Rule), allocator: std.mem.Allocator) !NumberType {
     var sum: NumberType = 0;
     for (sequences.items) |sequence| {
-        var correctedSequence = sequence;
-        var ruleApplied = true;
-        while (ruleApplied) {
-            ruleApplied = false;
-            for (rules.items) |rule| {
-                if (try enforceRule(correctedSequence, rule, allocator)) |newSequence| {
-                    correctedSequence = newSequence;
-                    ruleApplied = true;
-                    break;
-                }
-            }
-        }
-        sum += correctedSequence[correctedSequence.len / 2];
+        const corrected_sequence = try correctSequence(sequence, rules, allocator);
+        defer allocator.free(corrected_sequence);
+        sum += corrected_sequence[corrected_sequence.len / 2];
     }
     return sum;
+}
+
+fn correctSequence(sequence: Sequence, rules: std.ArrayList(Rule), allocator: std.mem.Allocator) !Sequence {
+    var current_sequence = try allocator.dupe(NumberType, sequence);
+    errdefer allocator.free(current_sequence);
+
+    var rule_applied = true;
+    while (rule_applied) {
+        rule_applied = false;
+        for (rules.items) |rule| {
+            if (try enforceRule(current_sequence, rule, allocator)) |new_sequence| {
+                allocator.free(current_sequence);
+                current_sequence = try allocator.dupe(NumberType, new_sequence);
+                rule_applied = true;
+                break;
+            }
+        }
+    }
+
+    return current_sequence;
 }
