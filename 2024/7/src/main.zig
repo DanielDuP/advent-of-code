a56af4690067320760d59ce5465993805977dab9
DanielDuP <54242914+DanielDuP@users.noreply.github.com>
2024-12-30 20:14:15 -0500

    day 7 - part 2

--- 2024/7/src/main.zig
+++ 2024/7/src/main.zig
@@ -16,67 +16,78 @@ pub fn main() !void {
 
     var line_buf: [2048]u8 = undefined;
 
-    var count: i64 = 0;
+    var basicCount: u64 = 0;
+    var advancedCount: u64 = 0;
 
-    var terms = std.ArrayList(i64).init(allocator);
+    var terms = std.ArrayList(u64).init(allocator);
     defer terms.deinit();
+
+    const basicOperators = [_]Operation{ .MULTIPLICATION, .ADDITION };
+    const advancedOperators = [_]Operation{ .MULTIPLICATION, .ADDITION, .CONCATENATION };
 
     while (try reader.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
         terms.clearRetainingCapacity();
         var splitLine = std.mem.split(u8, line, ":");
-        const target = try std.fmt.parseInt(i64, splitLine.next() orelse return error.InsufficientInput, 10);
+        const target = try std.fmt.parseInt(u64, splitLine.next() orelse return error.InsufficientInput, 10);
         var termIterator = std.mem.tokenizeScalar(u8, splitLine.next() orelse return error.InsufficientInput, ' ');
         while (termIterator.next()) |term| {
-            try terms.append(try std.fmt.parseInt(i64, term, 10));
+            try terms.append(try std.fmt.parseInt(u64, term, 10));
         }
-        if (try testSequence(terms.items, target, allocator)) {
-            count += target;
+        if (try testSequence(terms.items, target, &basicOperators, allocator)) {
+            basicCount += target;
+        }
+        if (try testSequence(terms.items, target, &advancedOperators, allocator)) {
+            advancedCount += target;
         }
     }
 
-    std.debug.print("Count: {}\n", .{count});
+    std.debug.print("basic count: {}\n", .{basicCount});
+    std.debug.print("advanced count: {}\n", .{advancedCount});
 }
 
 test {
     const allocator = testing.allocator;
 
     const TestCase = struct {
-        terms: []const i64,
-        target: i64,
+        terms: []const u64,
+        target: u64,
         expected: bool,
     };
 
     const testCases = [_]TestCase{
-        .{ .terms = &[_]i64{ 81, 40, 27 }, .target = 3267, .expected = true },
-        .{ .terms = &[_]i64{ 11, 6, 16, 20 }, .target = 292, .expected = true },
-        .{ .terms = &[_]i64{ 9, 7, 18, 13 }, .target = 21037, .expected = false },
-        .{ .terms = &[_]i64{ 16, 10, 13 }, .target = 161011, .expected = false },
+        .{ .terms = &[_]u64{ 81, 40, 27 }, .target = 3267, .expected = true },
+        .{ .terms = &[_]u64{ 11, 6, 16, 20 }, .target = 292, .expected = true },
+        .{ .terms = &[_]u64{ 9, 7, 18, 13 }, .target = 21037, .expected = false },
+        .{ .terms = &[_]u64{ 16, 10, 13 }, .target = 161011, .expected = false },
     };
+
+    const operators = [_]Operation{ .MULTIPLICATION, .ADDITION };
 
     for (testCases) |tc| {
         std.debug.print("Testing case: terms={any}, target={}\n", .{ tc.terms, tc.target });
-        const result = try testSequence(tc.terms, tc.target, allocator);
+        const result = try testSequence(tc.terms, tc.target, &operators, allocator);
         try testing.expectEqual(tc.expected, result);
     }
 }
 
-const Operation = enum {
-    MULTIPLICATION,
-    ADDITION,
-};
+const Operation = enum { MULTIPLICATION, ADDITION, CONCATENATION };
 
-const TermArray = []const i64;
+const TermArray = []const u64;
 
 fn apply(terms: TermArray, operation: Operation, allocator: std.mem.Allocator) !TermArray {
     assert(terms.len >= 2);
     const term1 = terms[0];
     const term2 = terms[1];
-    const result = switch (operation) {
+    const result: u64 = switch (operation) {
         .MULTIPLICATION => term1 * term2,
         .ADDITION => term1 + term2,
+        .CONCATENATION => blk: {
+            const shift = @as(u6, @intCast(std.math.log10_int(term2) + 1));
+            break :blk (term1 * std.math.pow(u64, 10, shift)) + term2;
+        },
     };
 
-    var new_terms = try ArrayList(i64).initCapacity(allocator, terms.len - 1);
+    var new_terms = try ArrayList(u64).initCapacity(allocator, terms.len - 1);
     errdefer new_terms.deinit();
 
     try new_terms.append(result);
@@ -85,18 +96,17 @@ fn apply(terms: TermArray, operation: Operation, allocator: std.mem.Allocator) !
     return new_terms.toOwnedSlice();
 }
 
-fn testSequence(terms: TermArray, target: i64, allocator: std.mem.Allocator) !bool {
+fn testSequence(terms: TermArray, target: u64, operators: []const Operation, allocator: std.mem.Allocator) !bool {
     if (terms.len == 1) {
         return terms[0] == target;
     }
 
-    const mult_result = try apply(terms, .MULTIPLICATION, allocator);
-    defer allocator.free(mult_result);
+    for (operators) |op| {
+        const result = try apply(terms, op, allocator);
+        defer allocator.free(result);
 
-    if (try testSequence(mult_result, target, allocator)) return true;
+        if (try testSequence(result, target, operators, allocator)) return true;
+    }
 
-    const add_result = try apply(terms, .ADDITION, allocator);
-    defer allocator.free(add_result);
-
-    return try testSequence(add_result, target, allocator);
+    return false;
 }
